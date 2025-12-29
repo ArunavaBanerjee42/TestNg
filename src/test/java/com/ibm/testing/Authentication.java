83160cb4a36a28b4211093fffc59ffef9fb2678b
thunderop9069-create <thunderop9069@gmail.com>
2025-12-29 12:57:40 +0530

    added a reusable loginHelper()

--- src/test/java/com/ibm/testing/Authentication.java
+++ src/test/java/com/ibm/testing/Authentication.java
@@ -9,14 +9,7 @@ import org.testng.annotations.Test;
 public class Authentication extends Setup{
   @Test (groups = {"authentication"})
   public void validLogin() throws InterruptedException {
-	  driver.findElement(By.linkText("Login")).click();
-	  WebElement loginform = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div"));
-	  if(loginform.isDisplayed()) {
-			driver.findElement(By.id("email")).sendKeys("arunavabanerjee2018@gmail.com");
-			driver.findElement(By.id("password")).sendKeys("123456");
-			driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/div[2]/form/button")).click();
-		}
-		Thread.sleep(4000);
+	    LoginHelper.login(driver, "arunavabanerjee2018@gmail.com", "123456");
 		String expectedTitle = "Dashboard";
 		String actualTitle = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/h1")).getText();
 		assertEquals(actualTitle, expectedTitle);
@@ -24,14 +17,7 @@ public class Authentication extends Setup{
   
   @Test (groups = {"authentication"})
   public void validEmailInvalidPasswordFormat() throws InterruptedException {
-	  driver.findElement(By.linkText("Login")).click();
-	  WebElement loginform = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div"));
-	  if(loginform.isDisplayed()) {
-			driver.findElement(By.id("email")).sendKeys("arunavabanerjee2018@gmail.com");
-			driver.findElement(By.id("password")).sendKeys("1236");
-			driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/div[2]/form/button")).click();
-		}
-		Thread.sleep(4000);
+	  	LoginHelper.login(driver, "arunavabanerjee2018@gmail.com", "1236");
 		String expectedErrorMessage = "Password must be at least 6 characters";
 		String actualErrorMessage = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/div[2]/form/div[2]/p")).getText();
 		assertEquals(expectedErrorMessage, actualErrorMessage);
@@ -39,14 +25,7 @@ public class Authentication extends Setup{
   
   @Test (groups = {"authentication"})
   public void validEmailInvalidPassword() throws InterruptedException {
-	  driver.findElement(By.linkText("Login")).click();
-	  WebElement loginform = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div"));
-	  if(loginform.isDisplayed()) {
-			driver.findElement(By.id("email")).sendKeys("arunavabanerjee2018@gmail.com");
-			driver.findElement(By.id("password")).sendKeys("123@36");
-			driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/div[2]/form/button")).click();
-		}
-		Thread.sleep(4000);
+	  	LoginHelper.login(driver, "arunavabanerjee2018@gmail.com", "12@@36");
 		String expectedErrorMessage = "Invalid credentials";
 		String actualErrorMessage = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div/div/div[2]/form/p[1]")).getText();
 		assertEquals(expectedErrorMessage, actualErrorMessage);
